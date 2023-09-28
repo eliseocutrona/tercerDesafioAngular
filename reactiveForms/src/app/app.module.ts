86ea3fc7b2a07ca1845a489eeb342fd0d11d0f22
eliseocutrona <eliseocutrona@gmail.com>
2023-09-28 00:17:52 -0300

    reactiveForms

--- reactiveForms/src/app/app.module.ts
+++ reactiveForms/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormularioReactivoComponent } from './components/formulario-reactivo/formulario-reactivo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioReactivoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
